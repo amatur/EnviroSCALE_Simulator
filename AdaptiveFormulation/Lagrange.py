fa1fcfb97fbd9a4281222e137145066ff96fcc27
amatur <amatur003@gmail.com>
2016-09-18 06:40:02 +0600

    By 1105003 after merge (06:35:08   18/09/16)

--- AdaptiveFormulation/Lagrange.py
+++ AdaptiveFormulation/Lagrange.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from scipy.optimize import minimize
 
 '''
 Lagrange multiplier augmented function:
@@ -10,47 +11,101 @@ f(x, y) = summation( T/p_i * v(p))
 g(x, y) = -(alpha + beta * sum(s_i/p_i) - rate * T)^2
 '''
 
-NUM_SENSOR = 4.0
-ROS = [1.0, 1.0,1.0, 1.0]               # ro_i,     depends on sensor
-GAMMAS = [0.08, 0.8, 2, 1]              # gamma_i,  depends on process/environment
-SENSOR_BYTES = [2.0, 5.0, 5.0, 4.0]     # s_i,      depends on bytes produced by a sensor
-S = SENSOR_BYTES
-ALPHA = 1000
-BETA = 3
-RATE = 10                           # rate,     upload rate, bytes/sec
+class LagrangeCalculator(object):
+    def __init__(self, sensors, rate, alpha, beta, lmbda):
+        '''
+        :param sensors: list of Sensor objects
+        :param rate: rate calculated by simulator (i.e. 100.0)  upload rate, bytes/sec
+        '''
+        self.sensors = sensors
+        self.num_of_sensor = len(sensors)
+        self.ALPHA = alpha
+        self.BETA = beta
+        self.RATE = rate
+        self.LAMBDA = lmbda
+        self.ROS = []                           # ro_i,     depends on sensor
+        self.GAMMAS = []                        # gamma_i,  depends on process/environment
+        self.S = []                             # s_i,      depends on bytes produced by a sensor
+        for i in range(0, self.num_of_sensor):
+            self.ROS.append(sensors[i].period)
+            self.GAMMAS.append(sensors[i].gamma)
+            self.S.append(sensors[i].size)
 
-def infoValue(p, ro_i, gamma_i):
-    return math.exp( -(p-ro_i) / gamma_i )
+    def infoValue(self, p, ro_i, gamma_i):
+        '''
+        :param p: Period (in second)
+        :param ro_i: Minimum Period (in second)
+        :param gamma_i: Scaling factor
+        :return: Information value of this sensor reading
+        '''
+        return math.exp( -(p-ro_i) / gamma_i )
 
-def func(X):
-    p0 = X[0]
-    p1 = X[1]
-    p2 = X[2]
-    p3 = X[3]
-    T = X[4]
-    L = X[5] # this is the multiplier. lambda is a reserved keyword in python
+    def func(self, X):
+        T = X[self.num_of_sensor]
+        # p0 = X[0]
+        # p1 = X[1]
+        # p2 = X[2]
+        # p3 = X[3]
+        # T = X[4]
+        # L = X[5] # this is the multiplier. lambda is a reserved keyword in python
+        # return -((T/p0)*self.infoValue(p0, self.ROS[0], self.GAMMAS[0]) \
+        #        + (T/p1)*self.infoValue(p1, self.ROS[1], self.GAMMAS[1]) \
+        #        + (T/p2)*self.infoValue(p0, self.ROS[2], self.GAMMAS[2]) \
+        #        + (T/p3)*self.infoValue(p0, self.ROS[3], self.GAMMAS[3]) \
+        #        - L* self.LAMBDA * math.pow((self.ALPHA + self.BETA * (  self.S[0]/p0 + self.S[1]/p1 + self.S[2]/p2 + self.S[3]/p3 ) - self.RATE * T), 2))
 
-    return -((T/p0)*infoValue(p0, ROS[0], GAMMAS[0]) \
-           + (T/p1)*infoValue(p1, ROS[1], GAMMAS[1]) \
-           + (T/p2)*infoValue(p0, ROS[2], GAMMAS[2]) \
-           + (T/p3)*infoValue(p0, ROS[3], GAMMAS[3]) \
-           - L * math.pow((ALPHA + BETA * (  S[0]/p0 + S[1]/p1 + S[2]/p2 + S[3]/p3 ) - RATE * T), 2))
+        # value_term, bytes_term is a generator
+        value_term = (((T / X[i]) * self.infoValue(X[i], self.ROS[i], self.GAMMAS[i])) for i in range(0, self.num_of_sensor))
+        bytes_term = ((self.S[i]/X[i]) for i in range(0, self.num_of_sensor))
+
+        # add the terms
+        sum_value_terms = 0.0
+        sum_bytes_terms = 0.0
+        for i in value_term:
+            sum_value_terms += i
+        for i in bytes_term:
+            sum_bytes_terms += i
+
+        return -( sum_value_terms - self.LAMBDA * math.pow(self.ALPHA + self.BETA * (sum_bytes_terms - self.RATE * T), 2) )
 
 
-from scipy.optimize import minimize
+    def tester(self):
 
-bnds = ((0.6, 3600), (0.2, 3600), (0.2, 3600), (0.2, 3600), (1, 60), (None, None))
-'''
-p0 >= 0.6
-p1 >= 0.2
-p2 >= 0.2
-p3 >= 0.2
-1 <= T <= 60
-'''
-X2 = minimize(func, [0, 0, 0, 0, 0, 9],  bounds=bnds)         #these are the initial guesses for p[i] and T
-#print "Max value: ", X2.x
-print "maximized value of the objective function", -func(list(X2.x))
+        ########################################
+        ## add the boundaries, example below ###
+        # bnds = ((self.ROS[0], 36000.0), (self.ROS[1], 36000.0), (self.ROS[2], 36000.0), (self.ROS[3], 36000.0), (0.01, None))
+        '''
+                p0 >= 0.6
+                p1 >= 0.2
+                p2 >= 0.2
+                p3 >= 0.2
+                1 <= T <= 60
+        '''
+        bnds = ()                                   # boundary for T (time period to upload)
+        for i in range(0, self.num_of_sensor):
+            bnds = bnds + ((self.ROS[i], 36000),)
+        bnds = bnds + ((0.01, None),)
+        ########################################
 
-for i in range(0,4):
-    print "Period", i, ": ", X2.x[i], " sec"
-print "T (Time Interval to Upload)", X2.x[4], "sec"
+
+        ########################################################
+        ## add the parameters initial guesses, example below ###
+        # example: [2.0, 2.0, 2.0, 2.0, 2.0, 0]
+        ########################################################
+        solver_input_list = []
+        for i in range(0, self.num_of_sensor):
+            solver_input_list.append(self.sensors[i].period)
+        INITIAL_T_GUESS = 10
+        solver_input_list.append(INITIAL_T_GUESS)
+        ########################################################
+
+        ########################################################
+        ## SOLVE
+        ########################################################
+        X2 = minimize(self.func, solver_input_list,  bounds=bnds)
+        print "maximized value of the objective function", -self.func(list(X2.x))
+
+        for i in range(0,self.num_of_sensor):
+            print "Period", i, ": ", X2.x[i], " sec"
+        print "T (Time Interval to Upload)", X2.x[self.num_of_sensor], "sec"
+        return X2.x
--- AdaptiveFormulation/Simulator.py
+++ AdaptiveFormulation/Simulator.py
@@ -1,50 +1,73 @@
 import heapq;
 import random;
 import Queue;
+import json
+from Lagrange import LagrangeCalculator
+
+############################################################################
+# Read from config #
+############################################################################
+try:
+    with open("sensor_config.json", 'r') as f:
+        c = json.load(f)
+except IOError:
+    print IOError
+    print("Error reading from config file: using default configuration")
+###########################################################################
+
 
 class Reading:
-    
     def __init__(self, sensor_name, sensing_time, size):
-	self.sensor_name = sensor_name
-	self.sensing_time = sensing_time
-	self.size = size
-    
+        self.sensor_name = sensor_name
+        self.sensing_time = sensing_time
+        self.size = size
+
     def __repr__(self):
         return 'Sensor::%s' % self.sensor_name
 
 
 class Callable:
     def call(sim):
-        raise Exception('Call of %s is not implemented' %self)
+        raise Exception('Call of %s is not implemented' % self)
 
 
 class Sensor(Callable):
-    def __init__(self, name, readlatency, period, size):
+
+    def __init__(self, name, readlatency, period, size, gamma):
+        """
+        Construct a new 'Sensor' object.
+        :param name: The name of Sensor
+        :param readlatency: read latency
+        :param period: The period to read
+        :param size: The size of sensor reading in bytes
+        :return: returns nothing
+        """
         self.name = name
         self.readlatency = readlatency
         self.period = period
         self.size = size
+        self.gamma = gamma
         self.flag = False
-        
+
     def __repr__(self):
         return 'Sensor::%s' % self.name
 
     def set_period(self, period):
-	self.period = period
-    
+        self.period = period
+
     def call(self, sim):
         if self.flag:
             self.flag = False
-            print 'Time %f sensor %s reading completed' %(sim.simclock, self)
+            print 'Time %f sensor %s reading completed' % (sim.simclock, self)
             sim.add_event(sim.simclock + self.period, self)
         else:
             self.flag = True
             sim.read_queue = sim.read_queue + self.size
-	    reading = Reading(self.name, sim.simclock, self.size)
-	    sim.readings_queue.put(reading)
+            reading = Reading(self.name, sim.simclock, self.size)
+            sim.readings_queue.put(reading)
             print 'Time %f reading sensor %s current queue %d' % (sim.simclock, self, sim.read_queue)
             sim.add_event(sim.simclock + self.readlatency, self)
-            
+
 
 class Uploader(Callable):
     def __init__(self, period, bandwidth, upload_rate, up_time, down_time):
@@ -53,148 +76,167 @@ class Uploader(Callable):
         self.upload_rate = upload_rate
         self.last_uploadtime = 0
         self.last_uploaded = 0
-        
+
         self.up_time = up_time
         self.down_time = down_time
-        
-        self.failed = False        
+
+        self.failed = False
         self.flag = False
-	self.currently_uploading = Queue.Queue()
-    
+        self.currently_uploading = Queue.Queue()
+
     def __repr__(self):
         return 'Uploader'
-        
+
     def call(self, sim):
         if self.failed:
 
             while not self.currently_uploading.empty():
-		reading = self.currently_uploading.get()
-		print '-----%f delay -- failed!' %(sim.simclock - reading.sensing_time)
+                reading = self.currently_uploading.get()
+                print '-----%f delay -- failed!' % (sim.simclock - reading.sensing_time)
 
             return
-            
+
         if self.flag:
             self.flag = False
-            print 'Time %f Upload completed' %sim.simclock
+            print 'Time %f Upload completed' % sim.simclock
 
-	    while not self.currently_uploading.empty():
-		reading = self.currently_uploading.get()
-		print '----- %f delay encountered' %(sim.simclock - reading.sensing_time)
+            while not self.currently_uploading.empty():
+                reading = self.currently_uploading.get()
+                print '----- %f delay encountered' % (sim.simclock - reading.sensing_time)
 
             sim.add_event(sim.simclock + self.period, self)
         else:
             bytes_to_upload = max(self.upload_rate * sim.simclock - self.last_uploaded, 0)
-	    bytes_to_be_uploaded = 0
+            bytes_to_be_uploaded = 0
 
-	    while (bytes_to_be_uploaded < bytes_to_upload) and not sim.readings_queue.empty():
-		reading = sim.readings_queue.get()
-		self.currently_uploading.put(reading)
-		bytes_to_be_uploaded = bytes_to_be_uploaded + reading.size
-	    bytes_to_upload = bytes_to_be_uploaded
-            
-	    upload_duration = 1.0 * bytes_to_upload / self.bandwidth
-            sim.read_queue = max(sim.read_queue - bytes_to_upload, 0)            
+            while (bytes_to_be_uploaded < bytes_to_upload) and not sim.readings_queue.empty():
+                reading = sim.readings_queue.get()
+                self.currently_uploading.put(reading)
+                bytes_to_be_uploaded = bytes_to_be_uploaded + reading.size
+            bytes_to_upload = bytes_to_be_uploaded
+
+            upload_duration = 1.0 * bytes_to_upload / self.bandwidth
+            sim.read_queue = max(sim.read_queue - bytes_to_upload, 0)
             self.last_uploaded = self.last_uploaded + bytes_to_upload
 
-            print 'Time %f Uploading %d bytes, remaining %d in queue' %(sim.simclock, bytes_to_upload, sim.read_queue)
+            print 'Time %f Uploading %d bytes, remaining %d in queue' % (sim.simclock, bytes_to_upload, sim.read_queue)
             self.flag = True
             sim.add_event(sim.simclock + upload_duration, self)
 
 
 class PeriodUpdater(Callable):
-    def __init__(self, param, time_gap):
-	self.param = param
-	self.interval = time_gap
+    def __init__(self, sensors, time_gap):
+        self.sensors = sensors
+        self.interval = time_gap
 
     def __repr__(self):
-	return "Period updater"
+        return "Period updater"
 
     def calculate_periods(self):
-	#...compute the periods...
-	return 5, 5, 5
-	
+        # ...compute the periods...
+        rate = 0.0
+
+        for s in self.sensors:
+            rate = rate + 1.0 * s.size / s.period
+        print "rate ", rate
+        l = LagrangeCalculator(self.sensors, rate, c["params"]["alpha"], c["params"]["beta"], c["params"]["lambda"])
+        x = l.tester()
+        return x[0], x[1], x[2]
+        # return 5, 5, 5
+
     def call(self, sim):
-	i = 0
-	period_tuple = self.calculate_periods()
-	for p in period_tuple:
-	    sim.sensors[i].set_period(p)
-	    i = i + 1
-	sim.add_event(sim.simclock + self.interval, self)
+        i = 0
+        period_tuple = self.calculate_periods()
+        for p in period_tuple:
+            sim.sensors[i].set_period(p)
+            i = i + 1
+        sim.add_event(sim.simclock + self.interval, self)
 
 
 class FailureHandler(Callable):
     def __init__(self, uploader):
         self.uploader = uploader
         self.flag = False
-        
+
     def call(self, sim):
         if self.flag:
-            duration = random.expovariate(1.0/self.uploader.up_time)
+            duration = random.expovariate(1.0 / self.uploader.up_time)
             self.uploader.failed = False
             self.flag = False
             print 'Time %f Uploader Up' % sim.simclock
             sim.add_event(sim.simclock, self.uploader)
-            sim.add_event(sim.simclock + duration, self)            
+            sim.add_event(sim.simclock + duration, self)
         else:
-            duration = random.expovariate(1.0/self.uploader.down_time)
+            duration = random.expovariate(1.0 / self.uploader.down_time)
             self.uploader.failed = True
             print 'Time %f Uploader down' % sim.simclock
-            self.flag = True            
-            sim.add_event(sim.simclock + duration, self)   
-                    
+            self.flag = True
+            sim.add_event(sim.simclock + duration, self)
+
+
 class Simulator:
     def __init__(self, seed):
+
         self.simclock = 0.0
-        self.event_queue = []        
-	self.readings_queue = Queue.Queue()
+        self.event_queue = []
+        self.readings_queue = Queue.Queue()
         self.read_queue = 0
         random.seed(seed)
-    
+
     def set_endtime(self, time):
         self.endtime = time
-                
+
+
     def init_scene(self):
-        s1 = Sensor('temp_hum', 0.1, 10.0, 12)
-        s2 = Sensor('gps', 0.05, 2.0, 20)
-        s3 = Sensor('ch4', 0.01, 8.0, 8)
-        
-        self.sensors = [s1, s2, s3]
+        self.sensors = []
+        num_sensors = len(c["sensors"])
+        for i in range(0, num_sensors):
+            s1 = Sensor(c["sensors"][i]["name"], c["sensors"][i]["readlatency"], c["sensors"][i]["period"],
+                    c["sensors"][i]["size"], c["sensors"][i]["gamma"])
+            self.sensors.append(s1)
+
+
+        #s1 = Sensor(c["sensors"][0]["name"], c["sensors"][0]["readlatency"], c["sensors"][0]["period"], c["sensors"][0]["size"])
+        #s2 = Sensor(c["sensors"][1]["name"], c["sensors"][1]["readlatency"], c["sensors"][1]["period"], c["sensors"][1]["size"])
+        #s3 = Sensor(c["sensors"][2]["name"], c["sensors"][2]["readlatency"], c["sensors"][2]["period"], c["sensors"][2]["size"])
+        #self.sensors = [s1, s2, s3]
+
         rate = 0.0
-        
+
         for s in self.sensors:
-            rate = rate + 1.0 * s.size / s.period 
-        
+            rate = rate + 1.0 * s.size / s.period
+
         print 'Rate %f' % rate
-        
+
         u = Uploader(5, 100, 0.99 * rate, 3600, 10)
         f = FailureHandler(u)
-	p = PeriodUpdater(1, 20)
+        #p = PeriodUpdater(1, 20)
+        p = PeriodUpdater(self.sensors, 20)
 
-        self.add_event(0, s1)
-        self.add_event(0, s2)
-        self.add_event(0, s3)
+        for s in self.sensors:
+            self.add_event(0, s)
         self.add_event(0, u)
         self.add_event(0, f)
-	#self.add_event(5, p)       
+        self.add_event(5, p)
 
     def add_event(self, time, event):
         heapq.heappush(self.event_queue, (time, event))
-        
+
     def run(self):
         while len(self.event_queue) > 0:
             time, event = heapq.heappop(self.event_queue)
             if time > self.endtime:
                 break
-            #print 'Time %f Event %s' %(time, event)            
-            
+            # print 'Time %f Event %s' %(time, event)
+
             self.simclock = time
-            event.call(self)                
-   
+            event.call(self)
+
 
 if __name__ == '__main__':
     sim = Simulator(123)
     sim.set_endtime(1000)
-    
+
     sim.init_scene()
     sim.run()
-                    
+
